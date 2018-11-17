3248c57032aa6107e359efea81f6dfc9aaf70487
maiderdorron <maider.dorronsoro@opendeusto.es>
2018-11-17 12:24:03 +0100

    aslkdñfj

--- src/LP/clsInicialUsuario.java
+++ src/LP/clsInicialUsuario.java
@@ -13,6 +13,8 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JSplitPane;
 import javax.swing.SwingConstants;
 
+import LP.clsPanelFondo;
+
 import java.awt.Font;
 import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
@@ -51,30 +53,7 @@ public class clsInicialUsuario extends JFrame {
 		setBackground(Color.WHITE);
 		getContentPane().setLayout(new BorderLayout(0, 0));
 		
-		JSplitPane splitPane = new JSplitPane();
-		splitPane.setResizeWeight(10.0);
-		splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
-		getContentPane().add(splitPane, BorderLayout.CENTER);
-		
-		JPanel pSuperior = new JPanel();
-		splitPane.setLeftComponent(pSuperior);
-		
-		
-		JButton bSignIn =new JButton("Sign In");
-		JButton bEmpresa =new JButton("Soy una Empresa");
-		JTextField tCorreo= new JTextField();
-		JTextField tPassword= new JTextField();
-	
-		pSuperior.add(tCorreo);
-		pSuperior.add(tPassword);
-		pSuperior.add(bSignIn);
-		tCorreo.setColumns(10);
-		tPassword.setColumns(10);
-		bSignIn.setBackground(Color.GRAY);
-		pSuperior.add(bEmpresa);
-	
-		clsPanelFondo pPrincipal = new clsPanelFondo();
-		splitPane.setLeftComponent(pPrincipal);
+
 		
 //		clsPanelFondo pPrincipal=new clsPanelFondo();
 //		splitPane.setRightComponent(pPrincipal);
