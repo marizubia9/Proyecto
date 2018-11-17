1cf584ffbd3aa98ae3694dc04e0103463507d6b1
marizubia9 <m.zubia@opendeusto.es>
2018-11-17 12:00:34 +0100

    probaconflicto

--- src/LP/clsMenuPrincipal.java
+++ src/LP/clsMenuPrincipal.java
@@ -1,6 +1,9 @@
 package LP;
 
+
+
 import javax.swing.JButton;
+import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -17,19 +20,24 @@ import java.awt.FlowLayout;
 import javax.swing.SwingConstants;
 
 
+
+
 import java.awt.Dialog.ModalExclusionType;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class clsMenuPrincipal extends JFrame
 {
 	
 	clsPanelFondo pPrincipal;
-	
+
 	JPanel pLogo;
 
 	
 	public clsMenuPrincipal ()
 	{
+		
 		setTitle("DOALZU");
 		setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
 		setSize( 800, 600 );
@@ -60,6 +68,19 @@ public class clsMenuPrincipal extends JFrame
 		
 		Image Doalzu;
 		Doalzu = Toolkit.getDefaultToolkit().getImage("C:\\Users\\ALUMNO\\workspace\\Proyecto\\src\\img\\DOALZU.jpg");
+		
+//		bSignIn.addActionListener(new ActionListener() 
+//		{
+//			public void actionPerformed(ActionEvent e)
+//			{
+//				clsMenuRopa clsMenuRopa=new clsMenuRopa();
+//				contentPane.setVisible(false);
+//				contentPane.add(clsMenuRopa);
+//				clsMenuRopa.setVisible(true);
+//				clsMenuRopa.toFront();
+//				contentPane.setVisible(true);
+//			}
+//		});
 		
 		
 	    }
