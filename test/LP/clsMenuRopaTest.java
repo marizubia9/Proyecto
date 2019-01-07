158c97e424f3424521b6bc1e78893cee2fca7129
marizubia9 <m.zubia@opendeusto.es>
2019-01-07 14:03:04 +0100

    JUnit prestatu

---
+++ test/LP/clsMenuRopaTest.java
@@ -0,0 +1,37 @@
+package LP;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ud.prog3.pr01.ListaDeReproduccion;
+
+/**
+ * Clase para comprobaciones Junit
+ * @author ALUMNO
+ *
+ */
+public class clsMenuRopaTest {
+
+	clsMenuRopa lr1;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		lr1 = new clsMenuRopa();
+	}
+
+	@After
+	public void tearDown() {
+		
+	}
+	
+	@Test
+	public void test() {
+		
+	}
+
+}
